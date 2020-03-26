c6e9fc7bc46026e77171dc65bbff06d1abc007f9
Alina Kornilova <alinakornilova78@gmail.com>
2020-03-26 17:31:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,16 +12,8 @@ function handleClick(e) {
 function greet(time) {
   const hour = parseInt(time, 10);
   if (hour < 12) return "Good Morning" 
-  if (hour > 17) return "Good Morning"  
-  console.log(`Wake ${dogName} the ${dogBreed}`);
-  console.log(`Leash ${dogName} the ${dogBreed}`);
-  console.log(`Walk to the park ${dogName} the ${dogBreed}`);
-  console.log(`Throw the fribsee for ${dogName} the ${dogBreed}`);
-  console.log(`Walk home with ${dogName} the ${dogBreed}`);
-  console.log(`Unleash ${dogName} the ${dogBreed}`);
-  return `${dogName} is happy and tired!`
+  if ( hour > 17) return "Good Evening"
+  return "Good Afternoon"
+
 }
- 
-let result = exerciseDog("Byron", "poodle");
-console.log(result);
 /* Write your implementation of displayMessage() */
