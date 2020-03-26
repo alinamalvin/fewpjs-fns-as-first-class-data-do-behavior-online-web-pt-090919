1ef48eb42c5888c1f66bf942db1b7669109a7fbb
Alina Kornilova <alinakornilova78@gmail.com>
2020-03-26 17:07:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,5 +8,21 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
-
+let weatherToday = "Rainy";
+ 
+function exerciseDog(string) {
+  if (weatherToday === "Rainy") {
+    return `${dogName} did not exercise due to rain`;
+  }
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+  console.log(`Leash ${dogName} the ${dogBreed}`);
+  console.log(`Walk to the park ${dogName} the ${dogBreed}`);
+  console.log(`Throw the fribsee for ${dogName} the ${dogBreed}`);
+  console.log(`Walk home with ${dogName} the ${dogBreed}`);
+  console.log(`Unleash ${dogName} the ${dogBreed}`);
+  return `${dogName} is happy and tired!`
+}
+ 
+let result = exerciseDog("Byron", "poodle");
+console.log(result);
 /* Write your implementation of displayMessage() */
